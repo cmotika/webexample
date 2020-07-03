6495146b31ad8fd9bd6b311e0dd308b9eb2b433e
Christian Motika <cmot@informatik.uni-kiel.de>
2020-07-03 18:11:53 +0200

    first commit

---
+++ webexample.js
@@ -0,0 +1,10 @@
+var express = require('express');  
+var app = express();  
+app.get('/', function (req, res) {  
+   res.send('Hello Lufthansa');  
+})  
+var server = app.listen(8888, function () {  
+var port = server.address().port  
+  //http://127.0.0.1:8888/
+ console.log("WebExample: Server up at http://127.0.0.1:%s", port)  
+})
